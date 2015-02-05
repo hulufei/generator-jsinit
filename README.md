adb3fd6d7887bb0758fac9a8bc527d82872fd8c7
hulufei <f.calabash@gmail.com>
2015-02-05 08:39:31 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# generator-jsinit [![Build Status](https://secure.travis-ci.org/hulufei/generator-jsinit.png?branch=master)](https://travis-ci.org/hulufei/generator-jsinit)
+
+> [Yeoman](http://yeoman.io) generator
+
+初始化一个 JS 项目，生成一些必要的配置文件：
+
+- .editorconfig
+- .jshintrc（参考 [jshint-practice](https://github.com/popomore/jshint-practice)）
+
+## MAYBE
+
+有其他需要可以借鉴
+[generator-sublime](https://github.com/thaiat/generator-sublime)
---
+++ app/index.js
@@ -0,0 +1,30 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the astonishing' + chalk.red('Jsinit') + ' generator!'
+    ));
+  },
+
+  writing: {
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  }
+});
